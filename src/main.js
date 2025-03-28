fba4b26492547befb8fb9c11835248ce7fda0222
asean-onpu <asean.onpu.ex@gmail.com>
2025-03-29 07:14:02 +0900

    分割導入

--- src/main.js
+++ src/main.js
@@ -87,17 +87,37 @@
 // }
 // console.log(func2("func2です"));
 
-// 引数二つを足す関数
-const func3 = (numA,numB) => {
-    let sum;
-    sum = numA + numB;    
-    return sum;
-}
+// // 引数二つを足す関数
+// const func3 = (numA,numB) => {
+//     let sum;
+//     sum = numA + numB;    
+//     return sum;
+// }
 
-const A = 10;
-const B = 20;
+// const A = 10;
+// const B = 20;
 
-console.log(`${A}+${B}=` + func3(A,B));
+// console.log(`${A}+${B}=` + func3(A,B));
 
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//     name : "じゃけぇ",
+//     age : 31
+// };
 
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`
+// console.log(message1);
 
+// const {name, age} = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`
+// console.log(message2);
+
+const myProfile = ["じゃけぇ", 31];
+const message3= `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`
+console.log(message3);
+
+const [name , age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}歳です。`
+console.log(message4);
