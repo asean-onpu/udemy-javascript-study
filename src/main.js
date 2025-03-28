35c06598c350489a1cb758079b611116b9f61b49
asean-onpu <asean.onpu.ex@gmail.com>
2025-03-29 06:40:03 +0900

    テンプレート文字列

--- src/main.js
+++ src/main.js
@@ -46,10 +46,25 @@
 // console.log(val4)
 
 // constで定義した配列はプロパティの変更が可能
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
 
+/**
+ * テンプレート文字列
+ */
+const name = "じゃけぇ";
+const age = 31;
+
+// 「私の名前はじゃけぇです。年齢は31歳です。」
+
+// 従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
+
+// テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
 
 
